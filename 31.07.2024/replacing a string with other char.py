664358567dcd7b17c7224951059de2110b2f3889
Darshana P.S <151154925+Dharshanapalanisamy@users.noreply.github.com>
2024-07-31 21:17:59 +0530

    Create replacing a string with other char.py

---
+++ 31.07.2024/replacing a string with other char.py
@@ -0,0 +1,5 @@
+def replace_char(s, old, new):
+    return s.replace(old, new)
+print(replace_char("hello world", "l", "x"))
+print(replace_char("test case", "t", "T"))   
+print(replace_char("banana", "a", "o"))       
