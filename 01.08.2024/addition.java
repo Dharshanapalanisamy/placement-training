d801996866d3b35a62e8e9d9b4780c635ea562ec
Darshana P.S <151154925+Dharshanapalanisamy@users.noreply.github.com>
2024-08-01 20:47:08 +0530

    Create addition.java

---
+++ 01.08.2024/addition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class a {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number : ");
+        int b=sc.nextInt();
+        System.out.println("sum = "+(a+b));
+    }
+}
