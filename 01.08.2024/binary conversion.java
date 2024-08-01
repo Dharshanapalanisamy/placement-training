2b5017e721e483c163ff7a7a3ca1d04139e645ca
Darshana P.S <151154925+Dharshanapalanisamy@users.noreply.github.com>
2024-08-01 21:02:48 +0530

    Create binary conversion.java

---
+++ 01.08.2024/binary conversion.java
@@ -0,0 +1,9 @@
+public class BinaryConversion
+  {
+    public static void main(String[] args)
+    {
+        int number = 10; 
+        String binaryString = Integer.toBinaryString(number);
+        System.out.println("The binary representation of " + number + " is: " + binaryString);
+    }
+}
